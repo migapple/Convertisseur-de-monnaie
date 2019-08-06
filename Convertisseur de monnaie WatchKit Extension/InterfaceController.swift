95c21a996ef622afd66f2d24d0daf5f8af215e55
Michel garlandat <michel.garlandat@me.com>
2019-08-06 18:09:26 +0200

    Initial Commit

---
+++ Convertisseur de monnaie WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Convertisseur de monnaie WatchKit Extension
+//
+//  Created by Michel Garlandat on 06/08/2019.
+//  Copyright © 2019 Michel Garlandat. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
